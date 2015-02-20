9ed1018d9c7197911deb84b93a475c6470a853b8
Mariam Mohamed Fawzy <maryammohamed61@gmail.com>
2015-02-20 16:19:46 +0200

    mariam : added some js to the posts

--- BlogTask/app/controllers/posts_controller.rb
+++ BlogTask/app/controllers/posts_controller.rb
@@ -13,14 +13,16 @@ class PostsController < ApplicationController
 			@posts_ids = []
 			@tags.each do |tag|
 				@category = Category.find_by(name: tag)
-				@temp_posts = @category.related_posts
-				if @temp_posts
-					@temp_posts.each do |p|
-						unless @posts_ids.include?(p[:id])
-							@posts_array << p
-							@posts_ids << p[:id]
-						end
-					end  
+				if @category
+					@temp_posts = @category.related_posts
+					if @temp_posts
+						@temp_posts.each do |p|
+							unless @posts_ids.include?(p[:id])
+								@posts_array << p
+								@posts_ids << p[:id]
+							end
+						end  
+					end
 				end
 			end
 			@posts = @posts_array
@@ -42,16 +44,19 @@ class PostsController < ApplicationController
 		end
 		@post_params[:user_id] = current_user.id
 		@post = Post.new(@post_params)
-		if @post.save
-			if (params[:post_attachments])
-				params[:post_attachments]['avatar'].each do |a|
-	            	@post_attachment = @post.post_attachments.create!(:avatar => a, :post_id => @post.id)
+		respond_to do |format|
+			if @post.save
+				if (params[:post_attachments])
+					params[:post_attachments]['avatar'].each do |a|
+		            	@post_attachment = @post.post_attachments.create!(:avatar => a, :post_id => @post.id)
+		        	end
 	        	end
-        	end
-			redirect_to user_path(current_user), notice: "The post has been posted successfully."
-		else
-			render action: :new, alert: "The post has not been posted."
-			redirect_to user_path(current_user)
+				format.html { redirect_to user_path(current_user), notice: "The post has been posted successfully."}
+				format.js
+			else
+				#render action: :new, alert: "The post has not been posted."
+				format.html { redirect_to user_path(current_user) }
+			end
 		end
 	end
 
