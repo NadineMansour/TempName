333a1d615a915f6435af22f06ec5fe124013d550
MMelouk94 <mmelouk94@gmail.com>
2015-02-21 18:43:04 +0200

    unauthorized users can't [post anymore

--- BlogTask/app/controllers/posts_controller.rb
+++ BlogTask/app/controllers/posts_controller.rb
@@ -37,50 +37,54 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		@post_params = post_params
-		if !(current_user.signature).nil?
-			@post_params[:body] = @post_params[:body] + " " + current_user.signature
-		end
-		@post_params[:user_id] = current_user.id
-		@related_categories = params[:search]
-		if @related_categories
-			@all_categories = @related_categories.split(',')
-		end
-		@categories = []
-		@categories_ids = []
-		if @all_categories
-			@all_categories.each do |c|
-				@temp_category = Category.find_by(name: c)
-				if @temp_category 
-					unless @categories_ids.include?(@temp_category.id)
-						@categories_ids << @temp_category.id
-						@categories << @temp_category
+		if current_user and current_user.auth
+			@post_params = post_params
+			if !(current_user.signature).nil?
+				@post_params[:body] = @post_params[:body] + " " + current_user.signature
+			end
+			@post_params[:user_id] = current_user.id
+			@related_categories = params[:search]
+			if @related_categories
+				@all_categories = @related_categories.split(',')
+			end
+			@categories = []
+			@categories_ids = []
+			if @all_categories
+				@all_categories.each do |c|
+					@temp_category = Category.find_by(name: c)
+					if @temp_category 
+						unless @categories_ids.include?(@temp_category.id)
+							@categories_ids << @temp_category.id
+							@categories << @temp_category
+						end
+					else
+						@new_category = Category.new
+						@new_category.name = c
+						@new_category.save
+						@categories_ids << @new_category.id
+						@categories << @new_category
 					end
-				else
-					@new_category = Category.new
-					@new_category.name = c
-					@new_category.save
-					@categories_ids << @new_category.id
-					@categories << @new_category
 				end
 			end
-		end
-		@post_params[:categories] = @categories
-		@post = Post.new(@post_params)
-		respond_to do |format|
-			if @post.save
-				if (params[:post_attachments])
-					params[:post_attachments]['avatar'].each do |a|
-		            	@post_attachment = @post.post_attachments.create!(:avatar => a, :post_id => @post.id)
+			@post_params[:categories] = @categories
+			@post = Post.new(@post_params)
+			respond_to do |format|
+				if @post.save
+					if (params[:post_attachments])
+						params[:post_attachments]['avatar'].each do |a|
+			            	@post_attachment = @post.post_attachments.create!(:avatar => a, :post_id => @post.id)
+			        	end
 		        	end
-	        	end
-				format.html { redirect_to user_path(current_user), notice: "The post has been posted successfully."}
-				format.js
-			else
-				#render action: :new, alert: "The post has not been posted."
-				format.html { redirect_to user_path(current_user) }
+					format.html { redirect_to user_path(current_user), notice: "The post has been posted successfully."}
+					format.js
+				else
+					#render action: :new, alert: "The post has not been posted."
+					format.html { redirect_to user_path(current_user) }
+				end
 			end
-		end
+		else
+			flash[:error] = "Something went wrong!"
+		end	
 	end
 
 	def edit
